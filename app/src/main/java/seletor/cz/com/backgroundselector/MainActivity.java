46620acb70586c4d0b8d2d0ff03956fa27a41bce
396640195 <396640195@qq.com>
2015-05-01 18:50:57 +0800

    update

--- app/src/main/java/seletor/cz/com/backgroundselector/MainActivity.java
+++ app/src/main/java/seletor/cz/com/backgroundselector/MainActivity.java
@@ -1,10 +1,12 @@
 package seletor.cz.com.backgroundselector;
 
 import android.app.Activity;
+import android.graphics.drawable.StateListDrawable;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.StateSet;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -27,8 +29,15 @@ public class MainActivity extends Activity {
             int padding = 5;
             view.setPadding(padding, padding, padding, padding);
             view.setScaleType(ImageView.ScaleType.FIT_CENTER);
-            view.setBackgroundResource(R.drawable.image_lock_item_bg_selector);
-            view.setImageResource(R.drawable.ic_launcher);
+            //1.利用该方法或以做多种效果
+            //view.setBackgroundResource(R.drawable.image_lock_item_bg_selector);
+            //view.setImageResource(R.drawable.ic_launcher);
+            //2.第二种，代码级别设置
+            StateListDrawable d = new StateListDrawable();
+            d.addState(new int[]{android.R.attr.state_pressed}, getResources().getDrawable(R.drawable.app_download_p));
+            d.addState(StateSet.WILD_CARD, getResources().getDrawable(R.drawable.app_download));
+            view.setImageDrawable(d);
+
             view.setActivated(true);
             view.setOnClickListener(new View.OnClickListener() {
                 @Override
